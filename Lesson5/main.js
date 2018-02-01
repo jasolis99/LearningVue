9428fb4dbfd7edddd47e40dece94a095aa44db03
Jose Alberto Solis <josealbertosolis@MacBook-Pro-de-Jose.local>
2018-02-01 19:34:43 +0100

    Lesson5

---
+++ Lesson5/main.js
@@ -0,0 +1,62 @@
+
+const vm = new Vue({
+
+    el: 'main',
+    data: {
+        mensaje: 'Hola Mundo',
+        nuevaTarea: null,
+        tareas:[{
+            titulo: 'Aprender Vue.js',
+            prioridad: true,
+            antiguedad: 23,
+
+        },
+        {
+            titulo: 'Aprender ES6',
+            prioridad: false,
+            antiguedad: 135,
+        },
+        {
+            titulo: 'Publicar algo todos los dias',
+            prioridad: true,
+            antiguedad: 378,
+        } 
+    ]
+    },
+
+    methods:{
+        agregarTarea(){
+
+            // console.log('Funciona')
+            // this, hace referencia a la instancia Vue
+            this.tareas.unshift(this.nuevaTarea);
+            this.nuevaTarea = null;
+
+
+        }
+    },
+
+    computed: {
+        mensajeAlReves(){
+            return this.mensaje.split('').reverse().join('');
+        },
+
+    //    tareasConPrioridad(){
+    //       return this.tareasConPrioridad.filter(function(tarea){
+    //         return tarea.prioridad;
+    //         });
+    //      },   
+    //  }
+        tareasConPrioridad(){
+           return this.tareas.filter((tarea) => tarea.prioridad);
+        },
+
+        tareasPorAntiguedad(){
+            return this.tareas.sort((a, b) => b.antiguedad - a.antiguedad);
+        }
+    
+    
+    
+    
+    },
+});
