94cfb15d5c915679811ae54c417315a0b5283620
Jose Alberto Solis <josealbertosolis@MacBook-Pro-de-Jose.local>
2020-09-19 20:34:30 +0200

    delete all

--- Vue-Router/Project-router/src/components/EquipoJose.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <!--This template is from Neasted Routes lesson-->
-    <div>
-        <h2>Jose Alberto Solis Godoy</h2>
-        <h5>josealbertosolis@evg.es</h5>
-    </div>
-</template>
